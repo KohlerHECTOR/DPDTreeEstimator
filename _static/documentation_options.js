b99f889336429a0d731b68ee16d9fb07394afb92
KohlerHECTOR <KohlerHECTOR@users.noreply.github.com>
2024-09-20 14:16:02 +0000

    [ci skip] re,oved prints ed583f360850dd747533b93ddb4e237c65ee26e2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev21+g0fa6d92.d20240920',
+    VERSION: '0.1.dev22+ged583f3.d20240920',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
