b216988149b3ca4007b18da6a2effd8fa48faafb
KohlerHECTOR <KohlerHECTOR@users.noreply.github.com>
2024-09-23 11:13:38 +0000

    [ci skip] update pic 418cfaade89a8b6850690d55a5f617684f3b7281

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev28+gc8c9ae8.d20240923',
+    VERSION: '0.1.dev29+g418cfaa.d20240923',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
