4d57420bf8a1dbc3b4b6fd9e6bbdbae30d0308c4
KohlerHECTOR <hector.kohler@inria.fr>
2024-09-24 15:47:14 +0200

    update patho_dataset fig

--- examples/plot_pathological_tree_classif.py
+++ examples/plot_pathological_tree_classif.py
@@ -4,6 +4,7 @@ A Pathological Dataset
 ============================
 A Dataset where the base :class:`sklearn.tree.DecisionTreeClassifier` fails.
 """
+from time import time
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -46,7 +47,7 @@ cm = plt.cm.RdBu
 cm_bright = ListedColormap(["#FF0000", "#0000FF"])
 
 # Create figure with 3 subplots
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 3))
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(16, 3))
 
 # Plot 1: Pathological dataset
 ax1.scatter(x[:, 0], x[:, 1], c=y, cmap=cm_bright, edgecolor="black")
@@ -57,26 +58,42 @@ ax1.set_xticks(())
 ax1.set_yticks(())
 
 # Plot 2: DecisionTreeClassifier
+start = time()
 tree = DecisionTreeClassifier(max_depth=3, random_state=0).fit(x, y)
+end = time() - start
+score_ = tree.score(x, y)
 DecisionBoundaryDisplay.from_estimator(tree, x, cmap=cm, alpha=0.8, ax=ax2, eps=0.5)
 # ax2.scatter(x[:, 0], x[:, 1], c=y, cmap=cm_bright, edgecolors="k", alpha=0.2)
-ax2.set_title("Decision Tree")
+ax2.set_title("Decision Tree\n Accuracy:{}%, Time: {}s".format(round(score_*100), round(end, 4)))
 ax2.set_xlim(0, 1)
 ax2.set_ylim(0, 1)
 ax2.set_xticks(())
 ax2.set_yticks(())
 
 # Plot 3: DPDTreeClassifier
+start = time()
 dpd_tree = DPDTreeClassifier(max_depth=3, random_state=0, cart_nodes_list=(8,)).fit(
     x, y
 )
+end = time() - start
+score_ = dpd_tree.score(x, y)
 DecisionBoundaryDisplay.from_estimator(dpd_tree, x, cmap=cm, alpha=0.8, ax=ax3, eps=0.5)
 # ax3.scatter(x[:, 0], x[:, 1], c=y, cmap=cm_bright, edgecolors="k", alpha=0.2)
 ax3.set_xlim(0, 1)
 ax3.set_ylim(0, 1)
 ax3.set_xticks(())
 ax3.set_yticks(())
-ax3.set_title("DP Decision Tree")
+ax3.set_title("DP Decision Tree\n Accuracy:{}%, Time: {}s".format(round(score_*100), round(end, 4)))
+
+
+DecisionBoundaryDisplay.from_estimator(dpd_tree, x, cmap=cm, alpha=0.8, ax=ax4, eps=0.5)
+# ax3.scatter(x[:, 0], x[:, 1], c=y, cmap=cm_bright, edgecolors="k", alpha=0.2)
+ax4.set_xlim(0, 1)
+ax4.set_ylim(0, 1)
+ax4.set_xticks(())
+ax4.set_yticks(())
+ax4.set_title("Opt Decision Tree\n Accuracy:100%, Time: 92s")
+
 
 # Adjust layout and save figure
 plt.tight_layout()
