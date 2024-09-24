080c767ddb05e6a9e8945727318bbbc3f8d9715b
KohlerHECTOR <KohlerHECTOR@users.noreply.github.com>
2024-09-24 11:39:17 +0000

    [ci skip] relaxed test for macos b6ce5980e38025a571087f7d48a0c784d0a60cb7

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev31+g01b5611.d20240923',
+    VERSION: '0.1.dev32+gb6ce598.d20240924',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
