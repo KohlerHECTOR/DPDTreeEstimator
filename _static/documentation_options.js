ee3f57410ccfebf46aadb0e2075e4d2452eccd20
KohlerHECTOR <KohlerHECTOR@users.noreply.github.com>
2024-09-26 10:13:22 +0000

    [ci skip] more strict test for reg 73afed598c088c9b319f05e660d3e11e50d28bf6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev39+g61e9c62.d20240925',
+    VERSION: '0.1.dev40+g73afed5.d20240926',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
