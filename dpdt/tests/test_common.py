a3e9c66d3777af43a986d0096560d2f6c2a361c2
KohlerHECTOR <hector.kohler@inria.fr>
2024-10-23 13:29:38 +0200

    re added tests

--- dpdt/tests/test_common.py
+++ dpdt/tests/test_common.py
@@ -14,41 +14,41 @@ from dpdt import (
 # than check_estimator
 
 
-# @pytest.mark.parametrize("max_depth", [2, 3])
-# @pytest.mark.parametrize("n_estimators", [2, 4])
-# @pytest.mark.parametrize(
-#     "cart_nodes_list",
-#     [
-#         (3,),
-#         (
-#             3,
-#             3,
-#         ),
-#     ],
-# )
-# @pytest.mark.parametrize("n_jobs_dpdt", [None, 4])
-# def test_check_estimator_gb(max_depth, n_estimators, cart_nodes_list, n_jobs_dpdt):
-#     check_estimator(
-#         GradientBoostingDPDTClassifier(
-#             max_depth=max_depth,
-#             n_estimators=n_estimators,
-#             cart_nodes_list=cart_nodes_list,
-#             n_jobs_dpdt=n_jobs_dpdt,
-#         )
-#     )
+@pytest.mark.parametrize("max_depth", [2, 3])
+@pytest.mark.parametrize("n_estimators", [2, 4])
+@pytest.mark.parametrize(
+    "cart_nodes_list",
+    [
+        (3,),
+        (
+            3,
+            3,
+        ),
+    ],
+)
+@pytest.mark.parametrize("n_jobs_dpdt", [None, 4])
+def test_check_estimator_gb(max_depth, n_estimators, cart_nodes_list, n_jobs_dpdt):
+    check_estimator(
+        GradientBoostingDPDTClassifier(
+            max_depth=max_depth,
+            n_estimators=n_estimators,
+            cart_nodes_list=cart_nodes_list,
+            n_jobs_dpdt=n_jobs_dpdt,
+        )
+    )
 
 
-# @pytest.mark.parametrize("max_depth", [2, 3])
-# @pytest.mark.parametrize("max_nb_trees", [1, 10])
-# @pytest.mark.parametrize("cart_nodes_list", [(3,), (3, 3, 1)])
-# @pytest.mark.parametrize("n_jobs", [None, 4])
-# def test_check_estimator(max_depth, max_nb_trees, cart_nodes_list, n_jobs):
-#     check_estimator(
-#         DPDTreeClassifier(max_depth, max_nb_trees, cart_nodes_list, n_jobs=n_jobs)
-#     )
-#     check_estimator(
-#         DPDTreeRegressor(max_depth, max_nb_trees, cart_nodes_list, n_jobs=n_jobs)
-#     )
+@pytest.mark.parametrize("max_depth", [2, 3])
+@pytest.mark.parametrize("max_nb_trees", [1, 10])
+@pytest.mark.parametrize("cart_nodes_list", [(3,), (3, 3, 1)])
+@pytest.mark.parametrize("n_jobs", [None, 4])
+def test_check_estimator(max_depth, max_nb_trees, cart_nodes_list, n_jobs):
+    check_estimator(
+        DPDTreeClassifier(max_depth, max_nb_trees, cart_nodes_list, n_jobs=n_jobs)
+    )
+    check_estimator(
+        DPDTreeRegressor(max_depth, max_nb_trees, cart_nodes_list, n_jobs=n_jobs)
+    )
 
 
 @pytest.mark.parametrize("max_depth", [2, 3])
