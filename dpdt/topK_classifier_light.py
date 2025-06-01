8b46e4fd4575e6f4ef5f06bf6bec3eb3bac12d7c
Hector Kohler <77987579+KohlerHECTOR@users.noreply.github.com>
2025-06-01 19:00:06 +0200

    Update topK_classifier_light.py

--- dpdt/topK_classifier_light.py
+++ dpdt/topK_classifier_light.py
@@ -9,7 +9,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils._param_validation import Interval, StrOptions
 from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.parallel import Parallel, delayed
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_is_fitted, validate_data
 
 
 def gini_impurity(y):
@@ -195,7 +195,7 @@ class TopKTreeClassifier(ClassifierMixin, BaseEstimator):
         self : object
             Returns self.
         """
-        X, y = self._validate_data(X, y)
+        X, y = self.validate_data(X, y)
         check_classification_targets(y)
 
         self._sample_weight = np.ones(len(X), dtype=np.float32)
@@ -484,7 +484,7 @@ class TopKTreeClassifier(ClassifierMixin, BaseEstimator):
             The predicted class labels.
         """
         check_is_fitted(self)
-        X = self._validate_data(X, reset=False)
+        X = self.validate_data(X, reset=False)
         return self._predict_zeta(X, -1)[0]  # just scores, not lengths
 
     def _predict_zeta(self, X, zeta_index):
