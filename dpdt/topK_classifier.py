7c9c0584c4a7c719628d448a100aabbbda923f61
Hector Kohler <77987579+KohlerHECTOR@users.noreply.github.com>
2025-06-01 19:18:29 +0200

    Update topK_classifier.py

--- dpdt/topK_classifier.py
+++ dpdt/topK_classifier.py
@@ -194,7 +194,7 @@ class TopKTreeClassifier(ClassifierMixin, BaseEstimator):
         self : object
             Returns self.
         """
-        X, y = self.validate_data(X, y)
+        X, y = validate_data(X, y)
         check_classification_targets(y)
 
         self._sample_weight = np.ones(len(X), dtype=np.float32)
@@ -467,7 +467,7 @@ class TopKTreeClassifier(ClassifierMixin, BaseEstimator):
             The predicted class labels.
         """
         check_is_fitted(self)
-        X = self.validate_data(X, reset=False)
+        X = validate_data(X, reset=False)
         return self._predict_zeta(X, -1)[0]  # just scores, not lengths
 
     def _predict_zeta(self, X, zeta_index):
